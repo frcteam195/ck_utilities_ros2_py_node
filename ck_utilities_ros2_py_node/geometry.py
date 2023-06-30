f629209830387877d7a7cb0b6c203f07ff95ea58
Chris Bonomi <bonomi.c11@gmail.com>
2023-06-30 01:31:14 +0000

    Fix geometry messages

--- ck_utilities_ros2_py_node/geometry.py
+++ ck_utilities_ros2_py_node/geometry.py
@@ -1,5 +1,5 @@
 import numpy
-from geometry_msgs.msg import *
+import geometry_msgs
 from nav_msgs.msg import *
 from std_msgs.msg import *
 import tf2_ros
@@ -13,10 +13,10 @@ class Translation:
         if input_type is None:
             self.__translation = numpy.zeros(3)
             return
-        elif isinstance(input_type, Vector3):
+        elif isinstance(input_type, geometry_msgs.msg._vector3.Vector3):
             self.__init_from_translation(input_type)
             return
-        elif isinstance(input_type, Point):
+        elif isinstance(input_type, geometry_msgs.msg._point.Point):
             self.__init_from_point(input_type)
             return
         elif isinstance(input_type, Translation):
@@ -26,13 +26,13 @@ class Translation:
             self.z = input_type.z
         raise ValueError("Type " + str(type(input_type)) + "is not supported by Translation constructor")
 
-    def __init_from_translation(self, input_translation : Vector3):
+    def __init_from_translation(self, input_translation : geometry_msgs.msg._vector3.Vector3):
         self.__translation = numpy.zeros(3)
         self.x = input_translation.x
         self.y = input_translation.y
         self.z = input_translation.z
 
-    def __init_from_point(self, input_point : Point):
+    def __init_from_point(self, input_point : geometry_msgs.msg._point.Point):
         self.__translation = numpy.zeros(3)
         self.x = input_point.x
         self.y = input_point.y
@@ -76,15 +76,15 @@ class Translation:
 
         return translation
 
-    def to_msg(self) -> Vector3:
-        output = Vector3()
+    def to_msg(self) -> geometry_msgs.msg._vector3.Vector3:
+        output = geometry_msgs.msg._vector3.Vector3()
         output.x = self.x
         output.y = self.y
         output.z = self.z
         return output
 
-    def to_msg_point(self) -> Point:
-        output = Point()
+    def to_msg_point(self) -> geometry_msgs.msg._point.Point:
+        output = geometry_msgs.msg._point.Point()
         output.x = self.x
         output.y = self.y
         output.z = self.z
@@ -96,10 +96,10 @@ class Rotation:
         if input_type is None:
             self.__rotation = numpy.zeros(3)
             return
-        elif isinstance(input_type, Vector3):
+        elif isinstance(input_type, geometry_msgs.msg._vector3.Vector3):
             self.__init_from_angular(input_type)
             return
-        elif isinstance(input_type, Quaternion):
+        elif isinstance(input_type, geometry_msgs.msg._quaternion.Quaternion):
             self.__init_from_quaternion(input_type)
             return
         elif isinstance(input_type, Rotation):
@@ -109,13 +109,13 @@ class Rotation:
             self.yaw = input_type.yaw
         raise ValueError("Type " + str(type(input_type)) + "is not supported by Rotation constructor")
 
-    def __init_from_angular(self, input_angular : Vector3):
+    def __init_from_angular(self, input_angular : geometry_msgs.msg._vector3.Vector3):
         self.__rotation = numpy.zeros(3)
         self.roll = input_angular.x
         self.pitch = input_angular.y
         self.yaw = input_angular.z
 
-    def __init_from_quaternion(self, input_quaternion : Quaternion):
+    def __init_from_quaternion(self, input_quaternion : geometry_msgs.msg._quaternion.Quaternion):
         self.__rotation = numpy.zeros(3)
         quat = ([input_quaternion.x, input_quaternion.y, input_quaternion.z, input_quaternion.w])
         eulers = euler_from_quaternion(quat)
@@ -148,15 +148,15 @@ class Rotation:
         quat = quaternion_from_euler(self.roll, self.pitch, self.yaw)
         return quaternion_matrix(quat)
 
-    def to_msg(self) -> Vector3:
-        output = Vector3()
+    def to_msg(self) -> geometry_msgs.msg._vector3.Vector3:
+        output = geometry_msgs.msg._vector3.Vector3()
         output.x = self.roll
         output.y = self.pitch
         output.z = self.yaw
         return output
 
-    def to_msg_quat(self) -> Quaternion:
-        output = Quaternion()
+    def to_msg_quat(self) -> geometry_msgs.msg._quaternion.Quaternion:
+        output = geometry_msgs.msg._quaternion.Quaternion()
         quats = quaternion_from_euler(self.roll, self.pitch, self.yaw)
         output.x = quats[0]
         output.y = quats[1]
@@ -170,12 +170,12 @@ class RotationQuaternion:
         if input_type is None:
             self.__rotation = numpy.zeros((4,4))
             return
-        elif isinstance(input_type, Quaternion):
+        elif isinstance(input_type, geometry_msgs.msg._quaternion.Quaternion):
             self.__init_from_quaternion(input_type)
             return
         raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
 
-    def __init_from_quaternion(self, input_quaternion : Quaternion):
+    def __init_from_quaternion(self, input_quaternion : geometry_msgs.msg._quaternion.Quaternion):
         self.__rotation = numpy.zeros(4,4)
         self.x = input_quaternion.x
         self.y = input_quaternion.y
@@ -216,7 +216,7 @@ class Pose:
             self.__position : Translation = Translation()
             self.__orientation : Rotation = Rotation()
             return
-        elif isinstance(input_type, Pose):
+        elif isinstance(input_type, geometry_msgs.msg._pose.Pose):
             self.__init_from_pose(input_type)
             return
         elif isinstance(input_type, Pose):
@@ -224,7 +224,7 @@ class Pose:
             self.__orientation : Rotation = Rotation(input_type.orientation)
         raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
 
-    def __init_from_pose(self, input_pose : Pose):
+    def __init_from_pose(self, input_pose : geometry_msgs.msg._pose.Pose):
         self.__position = Translation(input_pose.position)
         self.__orientation = Rotation(input_pose.orientation)
 
@@ -242,8 +242,8 @@ class Pose:
     def orientation(self, value : Rotation):
         self.__orientation = value
 
-    def to_msg(self) -> Pose:
-        output = Pose()
+    def to_msg(self) -> geometry_msgs.msg._pose.Pose:
+        output = geometry_msgs.msg._pose.Pose()
         output.position = self.__position.to_msg_point()
         output.orientation = self.__orientation.to_msg_quat()
         return output
@@ -257,12 +257,12 @@ class Transform:
             self.__linear : Translation = Translation()
             self.__angular : Rotation = Rotation()
             return
-        elif isinstance(input_type, Transform):
+        elif isinstance(input_type, geometry_msgs.msg._transform.Transform):
             self.__init_from_pose(input_type)
             return
         raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
 
-    def __init_from_pose(self, input_transform : Transform):
+    def __init_from_pose(self, input_transform : geometry_msgs.msg._transform.Transform):
         self.__linear = Translation(input_transform.translation)
         self.__angular = Rotation(input_transform.rotation)
 
@@ -280,8 +280,8 @@ class Transform:
     def angular(self, value : Rotation):
         self.__angular = value
 
-    def to_msg(self) -> Transform:
-        output = Transform()
+    def to_msg(self) -> geometry_msgs.msg._transform.Transform:
+        output = geometry_msgs.msg._transform.Transform()
         output.translation = self.__linear.to_msg()
         output.rotation = self.__angular.to_msg_quat()
 
@@ -291,19 +291,19 @@ class Twist:
             self.__linear : Translation = Translation()
             self.__angular : Rotation = Rotation()
             return
-        elif isinstance(input_type, Twist):
+        elif isinstance(input_type, geometry_msgs.msg._twist.Twist):
             self.__init_from_twist(input_type)
             return
-        elif isinstance(input_type, Transform):
+        elif isinstance(input_type, geometry_msgs.msg._transform.Transform):
             self.__init_from_pose(input_type)
             return
         raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
 
-    def __init_from_pose(self, input_transform : Transform):
+    def __init_from_pose(self, input_transform : geometry_msgs.msg._transform.Transform):
         self.__linear = Translation(input_transform.translation)
         self.__angular = Rotation(input_transform.rotation)
 
-    def __init_from_twist(self, input_transform : Twist):
+    def __init_from_twist(self, input_transform : geometry_msgs.msg._twist.Twist):
         self.__linear = Translation(input_transform.linear)
         self.__angular = Rotation(input_transform.angular)
 
@@ -321,8 +321,8 @@ class Twist:
     def angular(self, value : Rotation):
         self.__angular = value
 
-    def to_msg(self) -> Twist:
-        output = Twist()
+    def to_msg(self) -> geometry_msgs.msg._twist.Twist:
+        output = geometry_msgs.msg._twist.Twist()
         output.linear = self.__linear.to_msg()
         output.angular = self.__angular.to_msg()
         return output
